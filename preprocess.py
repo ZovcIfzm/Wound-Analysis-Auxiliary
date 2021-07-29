7f94768bc398e4b565da23dc2bbdcc906b19ae90
Alex Ruan <alexruan2222@gmail.com>
2021-07-28 22:05:14 -0400

    copied over basic masking from backend repo

---
+++ preprocess.py
@@ -0,0 +1,33 @@
+import cv2
+import numpy as np
+from scipy.spatial import distance as dist
+
+import wound_analysis.api.constants as k
+import copy
+
+from imutils import perspective
+from imutils import contours
+import imutils
+
+def apply_mask(lower_range, upper_range, image):
+    # Convert image to hsv file
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+    lower_mask = cv2.inRange(hsv, lower_range[0], upper_range[0])
+    upper_mask = cv2.inRange(hsv, lower_range[1], upper_range[1])
+    mask = cv2.bitwise_or(lower_mask, upper_mask)
+    image = cv2.bitwise_and(image, image, mask=mask)
+    image[mask > 0] = (255, 255, 255)
+
+    return image
+
+def sharpen(image):
+    kernel = np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]])
+    return cv2.filter2D(image, -1, kernel)
+
+def blur(image):
+    return cv2.GaussianBlur(image, (3, 3), 0)
+
+
+if __name__ == "__main__":
+    print("running as main file")
